44c4ade592dfb59f0d6bcb695e3673c7111f9d75
LegOleg5 <76555161+LegOleg5@users.noreply.github.com>
2023-04-25 17:23:42 +0300

    Update and rename main.py to server.py

--- server.py
+++ server.py
@@ -200,6 +200,6 @@ def check(solution_id, verdict):
 
 
 
-if __name__ == '__main__':
+if __name__ == '__server__':
     db_session.global_init("db/users.db")
     app.run(port=8080, host='127.0.0.1', debug=True)
